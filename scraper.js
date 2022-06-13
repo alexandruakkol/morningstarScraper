b92687bb3ab4c105c8295b094dca9103ced212d0
alexandruakkol <alexandru.akkol@gmail.com>
2022-06-13 12:22:52 +0300

    working quarterly balance sheet,  but with arbitrary timeout

--- scraper.js
+++ scraper.js
@@ -2,7 +2,7 @@ const puppeteer = require("puppeteer");
 
 async function scrapeIncomeStatement(symbol) {
   let url = `http://financials.morningstar.com/income-statement/is.html?t=${symbol}&region=usa&culture=en-US`;
-  console.log("Getting revenues for", symbol);
+  console.log("Getting income statement for", symbol);
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(url);
@@ -29,10 +29,98 @@ async function scrapeIncomeStatement(symbol) {
     return resultObj
     
   });
-
+  
   await browser.close();
   console.log(data);
 }
 
+async function scrapeBalanceSheet(symbol) {
+  const url = `http://financials.morningstar.com/balance-sheet/bs.html?t=${symbol}&region=usa&culture=en-US`
+  try {
+    const browser = await puppeteer.launch();
+    const [page] = await browser.pages();
+
+    await page.goto(url, { waitUntil: 'networkidle0' });
+    const data = await page.evaluate(() => {
+      
+      const codes = [
+      {'ttgg1':'cash'},
+      {'ttg1':'currentAssets'},
+      {'ttg2':'longtermAssets'},
+      {'tts1':'totalAssets'},
+      {'ttgg5':'currentLiabilities'},
+      {'ttgg6':'longtermLiabilities'},
+      {'ttg5':'totalLiabilities'},
+      {'ttg8':'totalStockEquity'},
+
+    ]
+    let tempArray = [];
+    let resultObj = {};
+    for (let code of codes){
+
+      for (let i=0; i<document.querySelector(`#data_${Object.keys(code)[0]}`).childNodes.length; i++){
+        tempArray.push(document.querySelector(`#data_${Object.keys(code)[0]}`).childNodes[i].innerHTML)
+      }
+      resultObj[Object.values(code)[0]] = tempArray;
+      tempArray = [];
+    }
+    return resultObj});
+
+    console.log(data);
+
+    await browser.close();
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function scrapeLastestBalanceSheet(symbol) {
+  const url = `https://financials.morningstar.com/balance-sheet/bs.html?t=${symbol}&region=usa&culture=en-US`
+  try {
+    const browser = await puppeteer.launch({headless: true});
+    const [page] = await browser.pages();
+
+    await page.goto(url, { waitUntil: 'networkidle0' });
+
+    await page.evaluate(() => SRT_stocFund.ChangeFreq(3,'Quarterly'))
+    //await page.waitForFunction("document.getElementById('type').innerText === 'Quarterly'")
+    await page.waitForTimeout(1500); 
+    
+    const data = await page.evaluate(() => {
+
+      
+
+      const codes = [
+      {'ttgg1':'cash'},
+      {'ttg1':'currentAssets'},
+      {'ttg2':'longtermAssets'},
+      {'tts1':'totalAssets'},
+      {'ttgg5':'currentLiabilities'},
+      {'ttgg6':'longtermLiabilities'},
+      {'ttg5':'totalLiabilities'},
+      {'ttg8':'totalStockEquity'},
+
+    ]
+    let tempArray = [];
+    let resultObj = {};
+    for (let code of codes){
+
+      for (let i=0; i<document.querySelector(`#data_${Object.keys(code)[0]}`).childNodes.length; i++){
+        tempArray.push(document.querySelector(`#data_${Object.keys(code)[0]}`).childNodes[i].innerHTML)
+      }
+      resultObj[Object.values(code)[0]] = tempArray;
+      tempArray = [];
+    }
+    return resultObj});
+
+    console.log(data);
+
+    await browser.close();
+  } catch (err) {
+    console.error(err);
+  }
+}
 symbol = "AAPL";
-scrapeIncomeStatement(symbol);
+//scrapeBalanceSheet(symbol);
+//scrapeIncomeStatement(symbol);
+scrapeLastestBalanceSheet(symbol)
