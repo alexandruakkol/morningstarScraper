32b350c414ec2d241ab9402de6c4733af66ef52f
alexandruakkol <alexandru.akkol@gmail.com>
2022-06-19 16:44:58 +0300

    finished computations

--- computation.js
+++ computation.js
@@ -1,35 +1,81 @@
-function computation (lastResult){
-    //deconstructing input
-    const { currentAssets, totalLiabilities, shares, price, totalStockEquity, netIncome } = lastResult;
+function computation(lastResult) {
+  //deconstructing input
+  const {
+    currentAssets,
+    totalLiabilities,
+    shares,
+    price,
+    totalStockEquity,
+    netIncome,
+    dividend,
+    ebitda,
+    revenues,
+    interestExpense,
+    longtermLiabilities,
+  } = lastResult;
 
-    let result = {};
-    let netNet = "N/A";
-    let mcap = "N/A";
-    let divy = "N/A";
-    let roe = "N/A";
-    let pe = "N/A";
-    let ebitdamg = "N/A";
-    let de = "N/A";
-    let interestrate = "N/A";
+  let result = {};
+  let netNet = "N/A";
+  let mcap = "N/A";
+  let divy = "N/A";
+  let roe = "N/A";
+  let pe = "N/A";
+  let ebitdaMg = "N/A";
+  let de = "N/A";
+  let interestRate = "N/A";
 
-    //netNet
-    if (currentAssets && totalLiabilities && shares && price) {
-      netNet = (price / ((currentAssets - totalLiabilities) / shares)).toFixed(2);
-    }
+  //netNet
+  if (currentAssets && totalLiabilities && shares && price) {
+    netNet = (price / ((currentAssets - totalLiabilities) / shares)).toFixed(2);
+  }
 
-    //MCap
-    if (price && shares) {
-        mcap = price*shares;
-      }
+  //MCap
+  if (price && shares) {
+    mcap = price * shares;
+  }
 
-    //roe
-    if (totalStockEquity && netIncome) {
-        roe = (netIncome/totalStockEquity).toFixed(2);
-      }
+  //roe
+  if (totalStockEquity && netIncome) {
+    roe = (netIncome / totalStockEquity).toFixed(2);
+  }
 
+  //divy
+  if (dividend && price) {
+    divy = (dividend / price).toFixed(4);
+  }
 
-    result = {...lastResult, 'netNet':netNet, mcap:mcap, roe:roe};
-    return result
+  //pe
+  if (mcap && netIncome) {
+    pe = (mcap / netIncome).toFixed(2);
+  }
+
+  //ebitdaMg
+  if (ebitda && revenues) {
+    ebitdaMg = (ebitda / revenues).toFixed(2);
+  }
+
+  //de
+  if (totalLiabilities && totalStockEquity) {
+    de = (totalLiabilities / totalStockEquity).toFixed(2);
+  }
+
+  //interestRate
+  if (interestExpense && longtermLiabilities) {
+    interestRate = (interestExpense / longtermLiabilities).toFixed(2);
+  }
+
+  result = {
+    ...lastResult,
+    netNet: netNet,
+    mcap: mcap,
+    roe: roe,
+    divy: divy,
+    pe: pe,
+    ebitdaMg: ebitdaMg,
+    de:de,
+    interestRate:interestRate
+  };
+  return result;
 }
 
 module.exports = computation;
